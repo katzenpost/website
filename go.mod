0357c766b4bbafc2a95e2106a6146411a7a08c4a
David Stainton <dstainton415@gmail.com>
2024-12-06 21:49:30 -0800

    wip

--- go.mod
+++ go.mod
@@ -2,4 +2,8 @@ module github.com/katzenpost/website
 
 go 1.23.0
 
-require github.com/google/docsy v0.11.0 // indirect
+require (
+	github.com/google/docsy v0.11.0 // indirect
+	github.com/google/docsy/dependencies v0.7.2 // indirect
+	github.com/hugomods/decap-cms v0.16.1 // indirect
+)
--- public/js/main.js
+++ public/js/main.js
@@ -6381,44 +6381,188 @@ limitations under the License.
 }(jQuery));
 
 ;
-/*
-Copyright 2018 Google LLC
-
-Licensed under the Apache License, Version 2.0 (the "License");
-you may not use this file except in compliance with the License.
-You may obtain a copy of the License at
-
-    https://www.apache.org/licenses/LICENSE-2.0
-
-Unless required by applicable law or agreed to in writing, software
-distributed under the License is distributed on an "AS IS" BASIS,
-WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-See the License for the specific language governing permissions and
-limitations under the License.
-*/
+// Adapted from code by Matt Walters https://www.mattwalters.net/posts/2018-03-28-hugo-and-lunr/
 
 (function ($) {
   'use strict';
 
-  var Search = {
-    init: function () {
-      $(document).ready(function () {
-        $(document).on('keypress', '.td-search input', function (e) {
-          if (e.keyCode !== 13) {
-            return;
-          }
+  $(document).ready(function () {
+    const $searchInput = $('.td-search input');
 
-          var query = $(this).val();
-          var searchPage = 'http://localhost:1313/search/?q=' + query;
-          document.location = searchPage;
+    //
+    // Register handler
+    //
 
-          return false;
+    $searchInput.on('change', (event) => {
+      render($(event.target));
+
+      // Hide keyboard on mobile browser
+      $searchInput.blur();
+    });
+
+    // Prevent reloading page by enter key on sidebar search.
+    $searchInput.closest('form').on('submit', () => {
+      return false;
+    });
+
+    //
+    // Lunr
+    //
+
+    let idx = null; // Lunr index
+    const resultDetails = new Map(); // Will hold the data for the search results (titles and summaries)
+
+    // Set up for an Ajax call to request the JSON data file that is created by Hugo's build process
+    $.ajax($searchInput.data('offline-search-index-json-src')).then((data) => {
+      idx = lunr(function () {
+        this.ref('ref');
+
+        // If you added more searchable fields to the search index, list them here.
+        // Here you can specify searchable fields to the search index - e.g. individual toxonomies for you project
+        // With "boost" you can add weighting for specific (default weighting without boost: 1)
+        this.field('title', { boost: 5 });
+        this.field('categories', { boost: 3 });
+        this.field('tags', { boost: 3 });
+        // this.field('projects', { boost: 3 }); // example for an individual toxonomy called projects
+        this.field('description', { boost: 2 });
+        this.field('body');
+
+        data.forEach((doc) => {
+          this.add(doc);
+
+          resultDetails.set(doc.ref, {
+            title: doc.title,
+            excerpt: doc.excerpt,
+          });
         });
       });
-    },
-  };
 
-  Search.init();
+      $searchInput.trigger('change');
+    });
+
+    const render = ($targetSearchInput) => {
+      //
+      // Dispose existing popover
+      //
+
+      {
+        let popover = bootstrap.Popover.getInstance($targetSearchInput[0]);
+        if (popover !== null) {
+          popover.dispose();
+        }
+      }
+
+      //
+      // Search
+      //
+
+      if (idx === null) {
+        return;
+      }
+
+      const searchQuery = $targetSearchInput.val();
+      if (searchQuery === '') {
+        return;
+      }
+
+      const results = idx
+        .query((q) => {
+          const tokens = lunr.tokenizer(searchQuery.toLowerCase());
+          tokens.forEach((token) => {
+            const queryString = token.toString();
+            q.term(queryString, {
+              boost: 100,
+            });
+            q.term(queryString, {
+              wildcard:
+                lunr.Query.wildcard.LEADING | lunr.Query.wildcard.TRAILING,
+              boost: 10,
+            });
+            q.term(queryString, {
+              editDistance: 2,
+            });
+          });
+        })
+        .slice(0, $targetSearchInput.data('offline-search-max-results'));
+
+      //
+      // Make result html
+      //
+
+      const $html = $('<div>');
+
+      $html.append(
+        $('<div>')
+          .css({
+            display: 'flex',
+            justifyContent: 'space-between',
+            marginBottom: '1em',
+          })
+          .append(
+            $('<span>').text('Search results').css({ fontWeight: 'bold' })
+          )
+          .append(
+            $('<span>').addClass('td-offline-search-results__close-button')
+          )
+      );
+
+      const $searchResultBody = $('<div>').css({
+        maxHeight: `calc(100vh - ${
+          $targetSearchInput.offset().top - $(window).scrollTop() + 180
+        }px)`,
+        overflowY: 'auto',
+      });
+      $html.append($searchResultBody);
+
+      if (results.length === 0) {
+        $searchResultBody.append(
+          $('<p>').text(`No results found for query "${searchQuery}"`)
+        );
+      } else {
+        results.forEach((r) => {
+          const doc = resultDetails.get(r.ref);
+          const href =
+            $searchInput.data('offline-search-base-href') +
+            r.ref.replace(/^\//, '');
+
+          const $entry = $('<div>').addClass('mt-4');
+
+          $entry.append(
+            $('<small>').addClass('d-block text-body-secondary').text(r.ref)
+          );
+
+          $entry.append(
+            $('<a>')
+              .addClass('d-block')
+              .css({
+                fontSize: '1.2rem',
+              })
+              .attr('href', href)
+              .text(doc.title)
+          );
+
+          $entry.append($('<p>').text(doc.excerpt));
+
+          $searchResultBody.append($entry);
+        });
+      }
+
+      $targetSearchInput.one('shown.bs.popover', () => {
+        $('.td-offline-search-results__close-button').on('click', () => {
+          $targetSearchInput.val('');
+          $targetSearchInput.trigger('change');
+        });
+      });
+
+      const popover = new bootstrap.Popover($targetSearchInput, {
+        content: $html[0],
+        html: true,
+        customClass: 'td-offline-search-results',
+        placement: 'bottom',
+      });
+      popover.show();
+    };
+  });
 })(jQuery);
 
 ;
